64005d3fa9caff68abc71db7b27b863739af4f96
Joao Pedro Greco <joaoppg1@al.insper.edu.br>
2026-07-28 10:01:12 -0300

    fifth function

--- funcoes.py
+++ funcoes.py
@@ -83,3 +83,11 @@ import random
 def sorteia_questao(questoes, nivel):
     return random.choice(questoes[nivel])
 
+def sorteia_questao_inedita(questoes, nivel, questoes_sorteadas):
+    questao = sorteia_questao(questoes, nivel)
+
+    while questao in questoes_sorteadas:
+        questao = sorteia_questao(questoes, nivel)
+
+    questoes_sorteadas.append(questao)
+    return questao
